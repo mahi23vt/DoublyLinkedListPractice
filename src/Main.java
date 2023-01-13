4cdd233f7295d2bf6a773ccf940423498a78c84d
mahi23vt <mahesh.23vt@gmail.com>
2023-01-13 14:38:48 +0530

    Version 2: updated with display

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    DoublyLinkedList list =  new DoublyLinkedList();
+
+    list.insertFirst(12);
+    list.insertFirst(24);
+    list.insertFirst(26);
+    list.display();
+    }
+}
