27123f838bf9996cff9007fff0c9d533513bbb1f
mahi23vt <mahesh.23vt@gmail.com>
2023-01-13 18:44:09 +0530

    Version 2: updated with : addToBack, addToIndex, goToNode, deleteFirst

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -17,7 +17,68 @@ public class DoublyLinkedList {
         head=node;
         size ++;
     }
+    public void insertLast(int num)
+    {
+        // creating a new node
+        NumberNode node = new NumberNode(new Number(num));
+        // check if it is the first element
+        if(tail == null) {
+            insertFirst(num);
+            return;
+        }
+        tail.next=node;
+        node.previous=tail;
+        node.next=null;
+        tail=node;
+        size ++;
 
+    }
+    public void insertToIndex(int num, int index)
+    {
+        if(index == 0)
+        {
+            insertFirst(num);
+            return;
+        }
+        if(index == size)
+        {
+            insertLast(num);
+            return;
+        }
+        NumberNode node = getNode(index-1);// Need to reach the previous node
+        NumberNode temp = node.next;
+        NumberNode newElement = new NumberNode(new Number(num),temp,node);
+        node.next=newElement;
+        temp.previous=newElement;
+        size++;
+    }
+
+    public NumberNode getNode(int index) // It returns the node present at that index value
+    {
+        NumberNode tempNode = head;
+        for(int i = 0;i<index;i++)
+        {
+            tempNode = tempNode.next;
+        }
+        return tempNode;
+    }
+    public void deleteFirst()
+    {
+        if(head == null)
+        {
+            System.out.println("No item in the list");
+            return;
+        }
+        NumberNode selectedNode = head;
+        head=head.next;
+        head.previous=null;
+        selectedNode.next=null;
+        size--;
+    }
+//    public void deleteLast()
+//    {
+//
+//    }
     public void display()
     {
         NumberNode tempNode = head;
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
     list.insertFirst(12);
     list.insertFirst(24);
     list.insertFirst(26);
+    list.insertLast(27);
+    list.display();
+    System.out.println(list.getNode(3).getNumber().getNum());
+    list.insertToIndex(20,3);
+    list.display();
+    list.deleteFirst();
     list.display();
     }
 }
